6d9258109650a10ad657a16bfcf63aaec6efc336
John Chacpi <johnchacpi479@gmail.com>
2023-03-23 14:50:27 -0500

    created custom hook for authentication, added new alias

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,15 @@
 import MainLayout from '@layout/MainLayout';
+import { ProviderAuth } from '@hooks/useAuth';
 import '@styles/globals.css';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <MainLayout>
-        <Component {...pageProps} />
-      </MainLayout>
+      <ProviderAuth>
+        <MainLayout>
+          <Component {...pageProps} />
+        </MainLayout>
+      </ProviderAuth>
     </>
   );
 }
