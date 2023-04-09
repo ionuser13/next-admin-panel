fdb6d32bfa3263373d7ca61099610736c812eb89
John Chacpi <johnchacpi479@gmail.com>
2023-04-08 22:45:22 -0500

    rendered headers depending on routes

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,17 +1,34 @@
 import React from 'react';
 import Header from '@components/Header';
 import Nav from '@common/Nav';
+import { useRouter } from 'next/router';
 
 export default function MainLayout({ children }) {
-  return (
-    <>
-      <div className="min-h-full">
-        <Header />
-        <Nav />
-        <main>
-          <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">{children}</div>
-        </main>
-      </div>
-    </>
-  );
+  const router = useRouter();
+  if(router.pathname === '/') {
+    return (
+      <>
+        <div className="min-h-full">
+          <Header />
+          <main>
+            <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">{children}</div>
+          </main>
+        </div>
+      </>
+    );
+  }
+  else {
+    return (
+      <>
+        <div className="min-h-full">
+          <Header />
+          <Nav />
+          <main>
+            <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">{children}</div>
+          </main>
+        </div>
+      </>
+    );
+  }
+  
 }
